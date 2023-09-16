a51d3bde05b9dd3b8f5f68b458a5eb67a48a19f3
SEJIN\sejin <dev021@4csoft.com>
2023-09-16 16:46:46 +0900

    스프링부트3 세팅 완료

---
+++ src/main/java/me/sejin/springbootdeveloper/SpringbootdeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.sejin.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdeveloperApplication.class, args);
+    }
+}
