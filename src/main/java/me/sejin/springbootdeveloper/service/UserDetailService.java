c0ad6dfbe492dd81e9b0991ab94098adf93239b3
SEJIN\sejin <dltpwls1203@gmail.com>
2023-09-21 00:44:29 +0900

    chap08

---
+++ src/main/java/me/sejin/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package me.sejin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.sejin.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
