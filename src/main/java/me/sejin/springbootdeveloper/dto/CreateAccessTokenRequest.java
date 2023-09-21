5c500b3cf3975190a0adf8443552d5228f5bbcb2
LeeSejin <dltpwls1203@gmail.com>
2023-09-21 16:29:58 +0900

    chap09

---
+++ src/main/java/me/sejin/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,11 @@
+package me.sejin.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
+
